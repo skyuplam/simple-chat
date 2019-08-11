c9456a35a75bac33b6af9328036e5b328c271156
Terrence Lam <skyuplam@gmail.com>
2019-08-11 15:37:37 +0200

    chore: add API_PORTOCOL and SECRET_KEY env var

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,15 @@ import { loadEnv } from './utils/env';
 
 loadEnv();
 
+export const API_PORTOCOL = process.env.API_PORTOCOL || 'http';
 export const API_PORT = process.env.API_PORT
   ? parseInt(process.env.API_PORT, 10)
   : 80;
 export const API_HOST = process.env.API_HOST || '0.0.0.0';
+
+export const SECRET_KEY = process.env.SECRET_KEY || 'n0s9d8fj3259jsdfiokinn980u98sfg09i0jsmdfguipjmi';
+
+export const API_ENDPOINT = [
+  [API_PORTOCOL, [API_HOST, API_PORT].join(':')].join('://'),
+  '/api',
+].join('');
