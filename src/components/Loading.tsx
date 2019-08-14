f6aaf3b48d1e9e7b6c2d1f7eee81e52e74b7078a
Terrence Lam <skyuplam@gmail.com>
2019-08-14 23:50:03 +0200

    feat: show loading animation when loading users

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
+import cn from 'clsx';
 import './Loading.css';
 
 
 interface Props {
   isLoading: boolean;
+  className?: string;
 }
-function Loading({ isLoading }: Props) {
+function Loading({ isLoading, className }: Props) {
   return isLoading ? (
-    <div className="lds-ring">
+    <div className={cn('lds-ring', className)}>
       <div />
       <div />
       <div />
--- src/components/Participants.tsx
+++ src/components/Participants.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import { isEmpty } from 'lodash';
 import { User } from 'SCModels';
 import List from './List';
 import ListItem from './ListItem';
+import Loading from './Loading';
+import './Participants.css';
 
 
 interface Props {
@@ -12,13 +15,19 @@ function Participants({
 }: Props) {
   return (
     <div className="Participants">
-      <List>
-        {users.map(user => (
-          <ListItem key={user.id}>
-            {user.name}
-          </ListItem>
-        ))}
-      </List>
+      {isEmpty(users) ? (
+        <div className="ParticipantsLoadingContainer">
+          <Loading isLoading={true} className="ParticipantsLoading" />
+        </div>
+      ) : (
+        <List>
+          {users.map(user => (
+            <ListItem key={user.id}>
+              {user.name}
+            </ListItem>
+          ))}
+        </List>
+      )}
     </div>
   );
 }
